705ffe0eacb81843ebffb7558dc0cbab74f6dbc4
unknown <IEUser@IE8Win7-04.cis.com>
2015-01-30 09:46:02 -0600

    Please enter the commit message for your changes. Lines starting 	modified:   Trivia/TriviaChallenge/TriviaChallenge.cpp

--- Trivia/TriviaChallenge/TriviaChallenge.cpp
+++ Trivia/TriviaChallenge/TriviaChallenge.cpp
@@ -1,4 +1,4 @@
-//Darien Hayes
+ //Darien Hayes
 //Danielle Brhely
 //Dennis Gordick
 // sran(time(NULL));
@@ -19,6 +19,7 @@ int main(void)
 	board();
 	return 0;
 }
+
 string space1_1 = "1";
 string space2_1 = "2";
 string space3_1 = "3";
@@ -33,6 +34,7 @@ string start_1 = "|Start";
 string start_2 = "|Start";
 string finish_1 = "Finish!|";
 string finish_2 = "Finish!|";
+
 int board(){
 	cout << "-------------------------------------------------------" << endl
 		<< start_1 << "|   " << space1_1 << "   |   " << space2_1 << "   |   " << space3_1 << "   |   " << space4_1 << "   |   " << space5_1 << "   |" << finish_1 << endl
@@ -42,8 +44,35 @@ int board(){
 	getchar();
 	return 0;
 }
+string question1 = " ";
+string question2 = " ";
+string question3 = " ";
+string question4 = " ";
+string question5 = " ";
+string question6 = " ";
+string question7 = " ";
+string question8 = " ";
+string question9 = " ";
+string question10 = " ";
 
 void questions(){
 	// To Do later
+
+	{cout << "What does a USB stand for? (Enter full name) ";
+		cin >> question1; }
+	{cout << "Are whales a mammal? (Yes or no) ";
+		cin >> question2; }
+	{cout << "Which operating system is more widley acceptable? (Windows or Mac OSX)";
+		cin >> question3; }
+	{cout << "What is Mr. Schmidt's first name? ";
+		cin >> question4; }
+	{cout << "What is 25 * 18 + 10 ";
+		cin >> question5; }
+	{cout << "What is the capitol of Missouri? ";
+		cin >> question6; }
+	{cout << "Which language is the easiest to learn? (Programming languages) ";
+		cin >> question7; }
+	{cout << "Which holiday in a year is usually busiest? ";
+		cin >> question8; }
 }
 
